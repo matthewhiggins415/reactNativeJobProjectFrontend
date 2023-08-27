4d555dfb15ce72f4993bf2237ca48f06db0bcb39
Matt Higgins <matt@apostrophecms.com>
2023-08-27 12:44:06 -0500

    added basic job components to index.js

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import { View, ScrollView, SafeAreaView } from 'react-native';
 import { Stack, useRouter } from 'expo-router';
 
 import { COLORS, icons, images, SIZES } from '../constants';
-import { NearbyJobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
 
 const Home = () => {
   const router = useRouter();
@@ -14,9 +14,29 @@ const Home = () => {
         options={{
           headerStyle: {
             backgroundColor: COLORS.lightWhite
-          }
+          },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ), 
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+          ), 
+          headerTitle: ""
         }}
       />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium
+          }}
+        >
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   )
 }
